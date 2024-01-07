34473d8791ecb58075c66e39e4bab5d350ea13c9
sina <sinamokhtari698@gmail.com>
2024-01-07 20:45:37 +0330

    end

--- home_app/models.py
+++ home_app/models.py
@@ -6,6 +6,7 @@ from django.db import models
 
 class Note(models.Model):
     text = models.CharField(max_length=100)
+    time = models.DateTimeField(auto_now_add=True, null=True, blank=True)
 
     def __str__(self):
         return self.text
